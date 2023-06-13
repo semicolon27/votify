b2225d477a2771303b38321f790feb5420972ee1
semicolon <hanzprakoso27@gmail.com>
2023-06-13 20:52:14 +0700

    ganti baseurl

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Pengaturan default untuk Axios
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://api.votify.lab.alfigufron.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
